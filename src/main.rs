aeaccc10ee09f3cedb6dcd3872ecb003c6655e03
developer1 <3rubasa@gmail.com>
2023-03-04 23:27:14 +0200

    Implement memory mapping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use std::ffi;
+use std::os::unix::raw;
 
 const O_RDWR: ffi::c_int = 2;
 const O_SYNC: ffi::c_int = 0x4000000;
+const PERIPH: ffi::c_int = 0x7E000000;
+const GPIO_OFFSET: ffi::c_int = 0x200000;
+const PROT_READ: ffi::c_int = 0x1;
+const PROT_WRITE: ffi::c_int = 0x2;
+const PROT_RDWR: ffi::c_int = PROT_READ | PROT_WRITE;
+const PAGE_SIZE: isize = 4096;
+
+const MAP_SHARED: ffi::c_int = 0x01;
 
 const GPIO_MEM_DEV: &str = "/dev/gpiomem";
 
@@ -17,6 +26,29 @@ fn main() {
     let fd = unsafe { open(pathname.as_ptr(), O_RDWR | O_SYNC) };
     println!("fd = {}", fd);
 
+    let addr = unsafe {
+        mmap(
+            0 as *const ffi::c_void,
+            PAGE_SIZE,
+            PROT_RDWR,
+            MAP_SHARED,
+            fd,
+            (PERIPH + GPIO_OFFSET) as raw::off_t,
+        )
+    };
+
+    if (addr as isize) < 0 {
+        println!("Failed to map memory!");
+        return;
+    }
+
+    println!("address = {}", addr as isize);
+
+    let res = unsafe { munmap(addr, PAGE_SIZE) };
+    if res < 0 {
+        println!("Failed to unmap memory!");
+    }
+
     unsafe {
         close(fd);
     };
@@ -25,4 +57,13 @@ fn main() {
 extern "C" {
     fn open(pathname: *const ffi::c_char, flags: ffi::c_int) -> ffi::c_int;
     fn close(fd: ffi::c_int);
+    fn mmap(
+        addr: *const ffi::c_void,
+        length: isize,
+        prot: ffi::c_int,
+        flags: ffi::c_int,
+        fd: ffi::c_int,
+        offset: raw::off_t,
+    ) -> *const ffi::c_void;
+    fn munmap(addr: *const ffi::c_void, length: isize) -> ffi::c_int;
 }
